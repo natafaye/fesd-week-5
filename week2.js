8f37843b60103d5af002f099eb4b0f59e8053dd5
Natalie Childs <nata.faye@gmail.com>
2023-02-08 21:10:04 -0800

    2/8/23 little examples

--- week2.js
+++ week2.js
@@ -1,48 +1,37 @@
 
-
-// if (true) {
-//     while (false) {
-//     }
-// }
-
-// if(something) {
-//     if(fdsfsd) {
-
-//     }
-// }
-
-// let isSunny = GETTING IT FROM THE USER
-// let isRainy = GETTING IT FROM THE USER
-
-// if(isSunny && isRainy || !isSunny) {
-//     alert("something")
-// }
-
-
-// let sunny = true;
-// let rainy = true;
-// if(sunny) {
-//     alert("Wear sunscreen!");
-// }
-// alert("You’re ready!");
-
-
-let numberOfCats = prompt("How many cats do you have?")
-
-numberOfCats = parseInt(numberOfCats)
-
-if(numberOfCats > 7) {
-    alert("You might have a cat addiction")
-}
-else if(numberOfCats === 3) {
-    alert("You need more cats")
-}
-else { // 1-7
-    alert("You have a great number of cats")
+let length = 23;
+if(length > 15 || length < 0) {
+    let maxLength = 15
+    length = maxLength;
 }
 
 
+let numberOfCats = prompt("How many cats?")
+for(let i = 0; i < numberOfCats; i++) {
+    alert("Petting the cat")
+}
 
+
+let guess = "";
+while(guess.toLowerCase() !== "pineapple") {
+    guess = prompt("Guess!");
+}
+alert("You guessed right!");
+
+
+let numCats = 0;
+while(numCats < 3) {
+    numCats++;
+}
+
+
+let something = 2
 for(let i = 0; i < 3; i++) {
-    alert("hello!")
+    if(something === 3) {
+        break;
+    }
+    if(i == 2) {
+        alert(i);
+    }
 }
+alert("You have enough!");
