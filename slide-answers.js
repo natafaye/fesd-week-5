defe22896763930d0b3ec7f60525cf20814cf453
Natalie Childs <nata.faye@gmail.com>
2023-06-13 21:19:49 -0700

    slide examples and problems

---
+++ slide-answers.js
@@ -0,0 +1,163 @@
+
+
+
+// Problem 1
+
+// for(let i = 1; i <= 10; i++) {
+//     console.log(i)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Problem 2
+
+// let message = ""
+
+// for(let i = 1; i <= 10; i++) {
+//   message += i
+// }
+
+// console.log(message)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Problem 3
+
+// let names = "The names were: "
+// let response = prompt("Enter a name")
+
+// while(response !== "done") {
+//   names += response + ", "
+//   response = prompt("Enter a name")
+// }
+
+// alert(names)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Problem 4
+
+// let numPeople = prompt("How many people?")
+// let isMorning = prompt("Is it morning?")
+// let message = ""
+
+// for(let i = 0; i < numPeople; i++) {
+//   if(isMorning === "yes") {
+//     message += "Good morning!\n"
+//   }
+//   else {
+//     message += "Good afternoon!\n"
+//   }
+// }
+
+// alert(message)
--- week2.js
+++ week2.js
@@ -1,37 +1,172 @@
 
-let length = 23;
-if(length > 15 || length < 0) {
-    let maxLength = 15
-    length = maxLength;
-}
+// let paysWell = true
+// let isLegal = false
+
+// let takeJob = paysWell && isLegal
+
+// alert(takeJob)
 
 
-let numberOfCats = prompt("How many cats?")
-for(let i = 0; i < numberOfCats; i++) {
-    alert("Petting the cat")
-}
+
+// dogName = "Milo"
+// likesFetch = true
+// sheds = true
+// pottyTrained = true
+// likesSnuggles = true
 
 
-let guess = "";
-while(guess.toLowerCase() !== "pineapple") {
-    guess = prompt("Guess!");
-}
-alert("You guessed right!");
+// // He wants it to be potty trained
+// // He wants it to like to snuggle or like to play fetch (or both)
+// // He doesn’t want it to shed
+
+// alert("Should he get Milo? " + (pottyTrained && !sheds && (likesSnuggles || likesFetch)))
+
+// dogName = "Penny"
+// likesFetch = true
+// sheds = false
+// pottyTrained = false
+// likesSnuggles = true
+
+// alert("Should he get Penny? " + (pottyTrained && !sheds && (likesSnuggles || likesFetch)))
 
 
-let numCats = 0;
-while(numCats < 3) {
-    numCats++;
-}
+// let age = 40
+// let country = "USA"
+// let money = 1
+
+// if ((age >= 21 || country === "Germany") && money >= 4.99) {
+//     alert("You can have it!")
+// }
 
 
-let something = 2
-for(let i = 0; i < 3; i++) {
-    if(something === 3) {
-        break;
+
+
+// let sunny = true
+// let rainy = true
+
+// if (sunny && rainy) {
+//     alert("Weird day!")
+// }
+// else if (sunny) {
+//     alert("Wear sunscreen!")
+// }
+// else if (rainy) {
+//     alert("Bring an umbrella!")
+// }
+// else { // implied else if(!rainy && !sunny)
+//     alert("What do we know") // we know that sunny and rainy are both false if we get here
+// }
+
+// alert("You’re ready!")
+
+
+// const age = prompt("What's your age?")
+// let grade = 0
+
+// switch (age) {
+//     case 6:
+//         grade = 1
+//         break
+//     case 7:
+//         grade = 2
+//         break
+//     case 6:
+//         grade = 1
+//         break
+//     case 8:
+//         grade = 1
+//         break
+//     case 9:
+//         grade = 1
+//         break
+//     case 6:
+//         grade = 1
+//         break
+// }
+
+
+// if(false) {
+//     alert("never run")
+// }
+
+// while(false) {
+//     alert("never run")
+// }
+
+
+
+// let guess = ""
+
+// while(guess.toLowerCase() !== "pineapple") { // guess !== "pineapple" && guess !== "Pineapple"
+//     guess = prompt("Guess!")
+// }
+
+// alert("You guessed right!")
+
+
+
+// total = 15
+// i = 6
+
+
+// let total = 0
+
+// for(let i = 1; i <= 5; i++) {
+//    total += i; 
+// }
+
+// alert("The total is " + total)
+
+
+// guess = "pineapple"
+
+// let guess = ""
+
+// do {
+//     guess = prompt("Guess!")
+// } while(guess !== "pineapple")
+
+// alert("You guessed right!")
+
+
+// pointsReset = true
+// points = 125
+// i = 31
+
+var points = 0
+var pointsReset = false
+
+
+
+// IMPORTANT: The code below is not fully functional, there were some tweaks we had to make on BJs screen to get it to match the expected output exactly
+// Can you get it working without watching the recording? :)
+
+
+for(let i = 0; i < 100; i++) { // i < 100 && points < 290
+    if(i % 2 === 0) {
+        points += 5
+    } else {
+        points += 3
     }
-    if(i == 2) {
-        alert(i);
+
+    // Wouldn't recommend - but it will work
+    // if(points === 125) {
+    //     if(!pointsReset) {
+    //         points = 25
+    //         pointsReset = true
+    //     }
+    // }
+
+    // Recommend this way
+    if(points === 125 && !pointsReset) { // pointsReset === false (another option to make that check)
+        points = 25
+        pointsReset = true
+    }
+
+    if(points >= 290) { // or you can do it with the emergency exit
+        break // emergency exit from looping or switch
     }
 }
-alert("You have enough!");
+  
+
+alert(points)
