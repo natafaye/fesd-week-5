b070da3770dffd5127687f1f8c2baf405cb1e9f5
Natalie Childs <nata.faye@gmail.com>
2023-08-09 18:41:10 -0700

    conditionals and loops

--- week2.js
+++ week2.js
@@ -1,11 +1,12 @@
+let dogName
+let likesFetch
+let sheds
+let pottyTrained
+let likesSnuggles
 
-let paysWell = true
-let isLegal = false
-
-let takeJob = paysWell && isLegal
-
-alert(takeJob)
-
+// He wants it to be potty trained
+// He wants it to like to snuggle or like to play fetch (or both)
+// He doesn’t want it to shed
 
 
 dogName = "Milo"
@@ -14,159 +15,77 @@ sheds = true
 pottyTrained = true
 likesSnuggles = true
 
-
-// He wants it to be potty trained
-// He wants it to like to snuggle or like to play fetch (or both)
-// He doesn’t want it to shed
-
-alert("Should he get Milo? " + (pottyTrained && !sheds && (likesSnuggles || likesFetch)))
+console.log(dogName)
+console.log(!sheds && pottyTrained && (likesSnuggles || likesFetch) )
 
 dogName = "Penny"
-likesFetch = true
+likesFetch = false
 sheds = false
-pottyTrained = false
+pottyTrained = true
 likesSnuggles = true
 
-alert("Should he get Penny? " + (pottyTrained && !sheds && (likesSnuggles || likesFetch)))
+console.log(dogName)
+console.log(!sheds && pottyTrained && (likesSnuggles || likesFetch) )
 
 
-let age = 40
+// = compares
+3 === 6 // false
+4 >= 2 // true
+
+let age
+
+// = assigns
+age = 3
+//+= is shorthand for =    +
+age += 2
+age = age + 2
+
+
+age = 40
 let country = "USA"
 let money = 1
 
-if ((age >= 21 || country === "Germany") && money >= 4.99) {
-    alert("You can have it!")
+let canDrink = (age >= 21 || country === "Germany") && money >= 4.99
+
+if((age >= 21 || country === "Germany") && money >= 4.99) {
+    alert("Here's a drink")
+} else {
+    alert("Go home")
 }
-
-
-
 
 let sunny = true
 let rainy = true
 
-if (sunny && rainy) {
-    alert("Weird day!")
-}
-else if (sunny) {
+if(sunny) {
     alert("Wear sunscreen!")
 }
-else if (rainy) {
+else if(rainy) {
     alert("Bring an umbrella!")
 }
-else { // implied else if(!rainy && !sunny)
-    alert("What do we know") // we know that sunny and rainy are both false if we get here
+else {
+    alert("Wear a coat!")
 }
 
-alert("You’re ready!")
+alert("You're ready!")
 
 
-age = prompt("What's your age?")
-let grade = 0
 
-switch (age) {
-    case 6:
-        grade = 1
-        break
-    case 7:
-        grade = 2
-        break
-    case 6:
-        grade = 1
-        break
-    case 8:
-        grade = 1
-        break
-    case 9:
-        grade = 1
-        break
-    case 6:
-        grade = 1
-        break
+
+
+let numCats = prompt("How many cats do you have?")
+
+// shame them for having a bad number of cats
+if(numCats < 2) {
+    alert("You don't have enough cats")
+} else if(numCats > 10) {
+    alert("You have a problem.")
+} else if(numCats > 4) {
+    alert("You have too many cats. Calm down.")
+}  else {
+    alert("We're friends")
 }
 
-
-if(false) {
-    alert("never runs")
-}
-
-while(false) {
-    alert("never runs")
-}
-
-
-
-let guess = ""
-
-while(guess.toLowerCase() !== "pineapple") { // guess !== "pineapple" && guess !== "Pineapple"
-    guess = prompt("Guess!")
-}
-
-alert("You guessed right!")
-
-
-
-// total = 15
-// i = 6
-
-
-let total = 0
-
-for(let i = 1; i <= 5; i++) {
-   total += i; 
-}
-
-alert("The total is " + total)
-
-
-
-
-guess = ""
-
-do {
-    guess = prompt("Guess!")
-} while(guess !== "pineapple")
-
-alert("You guessed right!")
-
-
-pointsReset = true
-points = 125
-i = 31
-
-var points = 0
-var pointsReset = false
-
-
-
-// IMPORTANT: The code below is not fully functional, there were some tweaks we had to make on BJs screen to get it to match the expected output exactly
-// Can you get it working without watching the recording? :)
-
-
-for(let i = 0; i < 100; i++) { // i < 100 && points < 290
-    if(i % 2 === 0) {
-        points += 5
-    } else {
-        points += 3
-    }
-
-    // Wouldn't recommend - but it will work
-    // if(points === 125) {
-    //     if(!pointsReset) {
-    //         points = 25
-    //         pointsReset = true
-    //     }
-    // }
-
-    // Recommend this way
-    if(points === 125 && !pointsReset) { // pointsReset === false (another option to make that check)
-        points = 25
-        pointsReset = true
-    }
-
-    if(points >= 290) { // or you can do it with the emergency exit
-        break // emergency exit from looping or switch
-    }
-}
+for(let i = 1; i <= 10; i++) {
+    console.log(i)
+  }
   
-
-alert(points)
