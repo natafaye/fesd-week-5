aa7926b6388c29df37b00ef697bd15fb7e4838e7
Natalie Childs <nata.faye@gmail.com>
2025-02-06 18:46:36 -0800

    cats and such

--- week5.js
+++ week5.js
@@ -1,63 +1,80 @@
-
-// let total = 4 + 3 // 7
-
-// let message = "How are you " + "Natalie" // "How are you Natalie"
-
-// let isAvailable = true && false // false
-
-// start on the right hand side of the equal sign
-// simplify that right hand side down to a value (string, number, boolean, null, undefined, etc)
-// That goes into the variable on the left hand side
+// Nothing data types
+// 1) null - intentional nothing
+// 2) undefined - never had anything
+// but they're basically the same thing
 
 
-/*** THE GUESSING GAME  ***/
-let correctNumber = 4
+// parseInt converts a string to a number
+parseInt("3") === 3 // true
+"3" === 3 // false
+// Using double equals == is bad and wrong and you will go to jail
 
-// Make guess and give it an empty string to start things out
-let guess = ""
 
-while(guess !== correctNumber) {
-    // a string of what the user typed in OR null
-    guess = prompt("Guess a number between 1 and 10")
 
-    // Potential bug: 4.xxxx is accepted
-    // some kind of if here to check if it's a decimal
-    // Sanitizing input = trying to stop users from breaking things
+// let howManyCats = prompt("How many cats do you have?") // Note: this will always be a string
 
-    // turn guess into a number
-    guess = parseInt(guess)
+// if(parseInt(howManyCats) === 0) {
+//     alert("How could you, they need a home!")
+// } else if(parseInt(howManyCats) > 100) {
+//     alert("Aaaaaaaah")
+// } else if(parseInt(howManyCats) > 10) { // if the code gets here, we know it's NOT greater than 100
+//     alert("I have questions")
+// } else { // it must not be zero and it must be less than 10
+//     alert("You're the best")
+// }
 
-    if(guess > correctNumber + 5) {
-        alert("Your number was WAY too high, calm down")
-    } else if(guess > correctNumber) { // Javascript will be a little buggy if they're strings so they're numbers
-        alert("Your number was too high")
-    } else if(guess < correctNumber) {
-        alert("Your number was too low")
-    } else if(isNaN(guess)) {
-        alert("That wasn't even a number bro")
+
+// i = 3
+
+// for(let i = 0; i < 3; i++) {
+//     alert("Here's a cat!")
+// }
+// alert("You have enough!")
+
+// total = 15
+// i = 6
+
+// Global Variables = variables created outside of any curly brackets
+// pros - it's available everywhere
+// con - it's everywhere, you can't really reuse that name anywhere
+let total = 0 // outside of any curly brackets - so it's completely free and available everywhere
+let special // not trapped inside the for loop curly brackets
+
+for(let i = 1; i <= 5; i++) {
+   total += i; // total = total + i
+   console.log(i)
+   special = "hi" 
+}
+console.log(special)
+
+console.log("The total is " + total)
+
+
+// total++ // total = total + 1
+// total += 2 // total = total + 2
+// total += 3 // total = total + 3
+
+// total++ // total += 1   total = total + 1
+
+// cost1 = 3
+// cost2 = 4
+// total += cost1
+// total += cost2
+
+
+// while(true) {
+//     // this will run forever and your computer will get loud and mad
+//     if(fdsfds) {
+//         break // emergency exits from the loop
+//     }
+// }
+
+
+for(let i = 0; i < 10; i++) {
+    if(i === 3) {
+        alert("You're a third of the way through")
+        while(false) {
+            // The sky's the limit
+        }
     }
 }
-
-// If they get out of the while loop and make it down here
-// then guess must be the correct number because that's
-// the only way that guess !== correctNumber can be false
-alert("You did it! That's the number!") 
-
-
-
-
-/*** FOR LOOP PRACTICE ***/
-
-let total = 0
-
-console.log(total) // 0
-
-for (let i = 0; i < 5; i++) {
-    console.log(total) // 0, 0, 1, 3, 6
-    total += i // total = total + i
-    console.log(total) // 0, 1, 3, 6, 10
-}
-
-console.log(total) // 10
-
-alert("The total is " + total)
