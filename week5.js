e6525ccdeebeb5ac183ad557996fbe4557c3b9e4
Natalie Childs <nata.faye@gmail.com>
2026-01-12 19:03:06 -0800

    sandwiches and numbers

--- week5.js
+++ week5.js
@@ -1,73 +1,115 @@
+let isFull = true
+let pizzaSlicesEaten = 0;
 
-// let userAnswer = prompt("What's your favorite food? 🍕 or 🍦 or 🍝")
+do {
+    isFull = true
+    pizzaSlicesEaten++ // pizzaSlicesEaten = pizzaSlicesEaten + 1
+    console.log("Eat a pizza slice! You have eaten: " + pizzaSlicesEaten)
+} while (!isFull)
 
-// if(userAnswer === "🍕") {
-//     alert("Correct answer!")
-// } else {
-//     alert("What is wrong with you")
+// 2 options for showing stuff to the user
+// neither of which are great right now
+// console.log()
+// alert()
+
+
+// While loop runs until the condition is false
+
+
+
+
+// Memory/variables:
+// guess = "pineapple"
+
+
+// let guess = ""
+
+// while(guess !== "pineapple") {
+//     guess = prompt("Guess!")
 // }
 
-
-// let numberOfFriends = prompt("How many friends are coming to the party?")
-
-// // 200
-// // Whoa calm down
-
-// // if friends is less than 3 then say how sad
-// if(numberOfFriends < 3) { // 0 - 2
-//     alert("how sad")
-// }
-// else if(numberOfFriends > 20) { // 21 - infinity
-//     alert("Whoa calm down")
-// }
-// // It can't get here without numberOfFriends being LESS OR EQUAL to 20
-// else if(numberOfFriends > 5) { // 6 - 20      && numberOfFriends <= 20
-//     alert("Perfect")
-// }
-// else { // 3, 4, 5
-//     alert("That's fine")
-// }
+// // If we've gotten to this line they MUST have typed in pineapple
+// alert("You guessed right!")
 
 
+// i = 3
+// cat alert, cat alert, cat alert
 
-// let numberOfFriends = 0 //prompt("How many friends are here right now?")
-// while(numberOfFriends < 10) {
-//     console.log("New arrival coming! Right now there are " + numberOfFriends + " friends")
-//     numberOfFriends++
-// }
-
-// for(let numberOfFriends = 0; numberOfFriends < 10; numberOfFriends++) {
-//     console.log("New arrival coming! Right now there are " + numberOfFriends + " friends")
-// }
-
-
-// for(let i = 0; i < 10; i++) {
-//     console.log("New arrival coming! Right now there are " + i + " friends")
-// }
-
-
-let partyLimit = prompt("How many people max can come to the party?")
-
-for (let i = 0; i < partyLimit; i++) {
-    console.log("New arrival coming! Right now there are " + i + " friends")
+let i = 0
+while (i < 3) {
+    alert("Here's a cat!")
+    i++ // i = i + 1
 }
+alert("You have enough!")
+console.log(i) // 3
 
 
 
-// Loops
-// Learning to read them so that you can write and debug them
-// Make sure you ask yourself what should be IN the loop and what should OUTSIDE the loop
+// cannot
+// can't
 
-let total = 0
+// i = i + 1
+// i++
 
-console.log(total) // CONSOLE: 0
+// part 1: very beginning once
+// part 2: check at the beginning of every iteration/loop
+// part 3: at the end of every iteration/looping
 
-for (let i = 1; i <= 5; i++) {
-    console.log(total) // CONSOLE: 0, 1, 3, 6, 10
-    total += i;
-    console.log(total) // CONSOLE:    1, 3, 6, 10, 15
-}
 
-console.log(total) // CONSOLE: 15
+// i = 3
+// cat alert, cat alert, cat alert
 
-alert("The total is " + total)
+// for(let i = 0; i < 3; i++) {
+//     alert("Here's a cat!")
+// }
+// alert("You have enough!")
+
+
+
+
+// MEMORY
+// total = 3
+// i = 3
+
+
+// If you had to guess, what does this code do?
+
+// let total = 0
+
+// console.log(total) // Console: 0
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log(total) // Console: 0, 1, 3, 6, 10
+//     total += i; // total = total + i
+//     console.log(total) // Console: 1, 3, 6, 10, 15
+// }
+
+// console.log(total) // Console: 15
+
+// alert("The total is " + total) // 15
+
+// Global Variable
+// Can be used anywhere, is never "out of scope"
+let number = 0
+
+if(true) {
+  number = 3
+  console.log(number) // Console: 3
+} // number gets thrown away
+
+console.log(number) // ERROR
+
+
+// Uncaught ReferenceError: something is not defined
+// =
+// something? Never heard of it!
+
+
+// For loop = we know how many times we're going to do it when we start
+// While loop = we know when to keep going
+
+// Making sandwiches for our friends that came over
+// For loop because we know how many friends we have
+
+// Doing the dishes
+// While loop because we know when to keep going
